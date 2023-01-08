95cb2742727bf6e18ef5ce520b3727a1b51f04fb
angelly1 <102198784+angelly1@users.noreply.github.com>
2023-01-08 15:48:54 -0600

    Add files via upload

---
+++ audi.py
@@ -0,0 +1,25 @@
+car = 'subaru'
+print("Is car == 'subaru'? I predict True.")
+print(car == 'subaru')
+
+print("\nIs car =='audi'? I predict False.")
+print(car=='audi')
+
+fruit = 'orange'
+print("\nDo you have an orange in your basket?")
+print(fruit == 'strawberry')
+
+age = 17
+if age >= 18:
+	print("You're old enough to vote!")
+	print("Have you registered to vote?")
+else:
+	print("Wait another year to be able to vote.")
+
+alien_color = "red"
+if alien_color == "green":
+	print("You've won 10 pts!")
+else:
+	print("You've won 5 pts!")
+
+rental
